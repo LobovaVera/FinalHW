7217b2fa275eb2144169c9c1823abb063e99e2d8
Vera Lobova <pchilli@yandex.ru>
2022-11-24 15:42:09 +0300

    Select method added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк длина которых меньше или равна 3
+
+void Main()
+{
+string[] ourArray = GetArray();
+PrintArray(ourArray);
+int maxSize = 3;
+string[] newArray = SelectMoreThanSize(ourArray, maxSize);
+Console.Write("\nСлова вашего массива, состоящие из трех или менее букв:");
+PrintArray(newArray);
+}
+
+string[] GetArray()
+{
+    Console.WriteLine("Введите несколько слов через пробел");
+    string ourLine = Console.ReadLine()!;
+    string[] array = ourLine.Split(' ');
+    return array;
+
+}
+
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    
+}
+
+string[] SelectMoreThanSize(string[] arr, int size)
+{ string line = string.Empty;
+int i = 0;
+    for (; i < arr.Length; i++)
+    {
+        if(arr[i].Length<=3)
+        {
+            line += arr[i] + ' ';
+        }
+    }
+    string[] resultArray = line.Split(' ');
+    return resultArray;
+}
+Main();
